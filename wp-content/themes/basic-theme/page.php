795bcf0b22dc3654bcffdaf8893dc599133ab2ac
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-04-29 20:46:45 +0300

    eds

--- wp-content/themes/basic-theme/page.php
+++ wp-content/themes/basic-theme/page.php
@@ -7,6 +7,11 @@
     <section class="site-page" <?php //if (is_active_sidebar('page-sidebar1')) : echo 'style="width:73%;"'; endif;
                                 ?>>
         <div class="page-header__inner">
+        <?php
+        if (has_post_thumbnail()) { // условие, если есть миниатюра
+            the_post_thumbnail('full'); // если параметры функции не указаны, то выводится миниатюра текущего поста, размер thumbnail
+        } 
+        ?>
             <div class="fixed-container">
                 <h2 class="site-page__title toopacity white"> <?php the_title(); ?> </h2>
             </div>

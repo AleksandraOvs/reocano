f0afab5a38f660ab687734f5d49ca6430e7f624d
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-04-29 20:09:21 +0300

    eds

--- wp-content/themes/basic-theme/page-books.php
+++ wp-content/themes/basic-theme/page-books.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The main template file
+ * Template name: Books
  *
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).

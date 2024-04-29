a37c8a3d20a7e05ab70f19b55c892859eda72f2a
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-04-29 20:37:36 +0300

    eds

--- wp-content/themes/basic-theme/archive-books.php
+++ wp-content/themes/basic-theme/archive-books.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * Template name: Books
- *
+ * 
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.

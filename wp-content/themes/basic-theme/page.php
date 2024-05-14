f8dabaf5a040014e1448791a2ea15197a90d9680
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-05-14 08:57:08 +0300

    eds

--- wp-content/themes/basic-theme/page.php
+++ wp-content/themes/basic-theme/page.php
@@ -18,16 +18,16 @@ get_header(); ?>
        
     <section class="site-page" <?php //if (is_active_sidebar('page-sidebar1')) : echo 'style="width:73%;"'; endif;
                                 ?>>
-         <div class="page-header__inner">
+         <!-- <div class="page-header__inner">
          <?php
-        if (has_post_thumbnail()) { // условие, если есть миниатюра
-            the_post_thumbnail('full'); // если параметры функции не указаны, то выводится миниатюра текущего поста, размер thumbnail
-        } 
+        // if (has_post_thumbnail()) { // условие, если есть миниатюра
+        //     the_post_thumbnail('full'); // если параметры функции не указаны, то выводится миниатюра текущего поста, размер thumbnail
+        // } 
         ?>
             <div class="fixed-container">
-                <h2 class="site-page__title toopacity white"> <?php the_title(); ?> </h2>
+                <h2 class="site-page__title toopacity white"> <?php //the_title(); ?> </h2>
             </div>
-        </div>
+        </div> -->
         <div class="full-width-container">
        <?php the_content() ?>
         </div>
